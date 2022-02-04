76b019e7f93f3334aad7f15ac6493f7b5eb92580
Bakazova Aida <solnyshko669@gmail.com>
2022-02-04 23:24:46 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,77 @@
+// function checkEven(a,b){
+//     return a + b
+// }
+//
+// console.log(checkEven(4, 34))
+
+// function checkEven (num){
+//     if (num % 2 === 0){
+//         return 'Even'
+//     }else {
+//         return 'Old'
+//     }
+// }
+//
+// console.log(checkEven(22))
+// console.log(checkEven(35))
+//
+// let str = "lorem ipsum"
+// if (str.includes("a")){
+//     console.log("yes")
+// }else{
+//     console.log("no")
+// }
+//
+// let str1 = "It Dubai"
+// if (str1.includes("a")){
+//     console.log("yes")
+// }else{
+//     console.log("no")
+// }
+//
+// let str2 = "Moskow"
+// if (str2.includes("a")){
+//     console.log("yes")
+// }else{
+//     console.log("no")
+// }
+
+// function task2(str){
+//     return `Hello my dear ${str}!`
+// }
+//
+// console.log(task2('Tima'))
+// console.log(task2('Sima'))
+// console.log(task2('Rina'))
+
+// function task3(value){
+//     return typeof value
+// }
+//
+// console.log(task3(30))
+
+// function task4(a, b){
+//     if(a > b){
+//         return "a bol'she b"
+//     }else if ( a < b){
+//         return "b bol'she a"
+//     }else {
+//         return "Oni ravny"
+//     }
+// }
+//
+// console.log(task4(34, 56))
+
+// function task7(num){
+//     return num.toString().length
+// }
+//
+// console.log(task7(127))
+
+function  task11(a, b, c, d){
+    return Math.min(a, b, c, d)
+
+}
+
+console.log(task11(5, 3, 2, 4))
+console.log(task11(121, 10, 2, 4))

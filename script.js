c67d1a9bfb91367ee145bf0c6fa01858e648ff2c
Bakazova Aida <solnyshko669@gmail.com>
2022-02-07 11:39:27 +0600

    next commit

--- script.js
+++ script.js
@@ -90,3 +90,69 @@
 // console.log(task2("Lena"))
 // console.log(task2("Maral"))
 // console.log(task2("Aya"))
+
+// Напишите функцию, которая принимает все, что угодно и возвращает его тип данных:
+
+// function task3 (value){
+//     return typeof value
+// }
+//
+// console.log(task3(65))
+// console.log(task3("Jhon"))
+
+// Напишите функцию, которая принимает два числа, если первое число больше второго, вывести: a больше b, если второе больше первого, то вывести : b больше a, иначе: Они равны
+// function task4 ( a, b ){
+//     if (a > b){
+//         return "a больше b"
+//     }else if ( a < b){
+//         return "b больше a"
+//     }
+//     return "Oni ravny"
+// }
+//
+// console.log(task4( 45, 98))
+// console.log(task4( 45, 34))
+// console.log(task4( 45, 45))
+
+// Напишите функцию, которая принимает число и проверяет его кратность пяти. Ответ представить в виде true или false:
+// function task5 (num){
+//     return num % 5 === 0
+// }
+//
+// console.log(task5 (62))
+
+// Напишите функцию, которая принимает строку и выводит длину строки:
+// function task5 (str){
+//     return str.length
+// }
+//
+// console.log(task5 ('Hello my love'))
+//Напишите функцию, которая принимает строку и выводит последний символ в этой строке:
+// function task6 (str){
+//     return str[str.length-1]
+// }
+//
+// console.log(task6('Bogatstvo'))
+// console.log(task6('Million'))
+
+//Напишите функцию, которая принимает число и выводит квадратный корень этого числа :
+// function task7 (num){
+//     return Math.sqrt(num)
+// }
+//
+// console.log(task7(25))
+//Напишите функцию, которая принимает число n и выводит целое число в диапазоне от -n до n :
+
+// function task8 (num){
+//     return Math.floor(Math.random()* 20 - num)
+// }
+//
+// console.log(task8(9))
+// Напишите функцию, которая принимает число n и выводит количество цифр в числе :
+
+// function task9 (num){
+//     return num.toString().length
+// }
+//
+// console.log(task9(65))
+
